b78f2b3f33b327d182b872933e19162d29a52634
Angie Vanegas <arco527@gmail.com>
2017-04-02 22:58:21 -0500

    Page without interactivity

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//BASE DE DATOS
+
+var productos = ["Abrigos", "Vestidos", "Chaquetas", "Overoles", "Gorras", "Bolsos"];
+var imgGrandes = ["images/caramng14.jpg","images/caramng15.jpg","images/caramng8.jpg"];
+var imgPequeñas = ["images/caa.jpg","images/cama.jpg","images/cach.jpg","images/cao.jpg","images/cag.jpg","images/cave.jpg"];
+var precios = [270, 220, 120, 70, 20, 200 ];
+var stock = [8, 15, 11, 3, 10, 22];
+var precioTransporte = [6, 12, 20, "gratis"];
+var IVA = 0.18;
+var uniser;
