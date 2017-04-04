4300db16514df0fa8c3e12bc64fff8583c1a87fd
Angie Vanegas <arco527@gmail.com>
2017-04-04 17:37:01 -0500

    without interactivity

--- js/app.js
+++ js/app.js
@@ -2,9 +2,53 @@
 
 var productos = ["Abrigos", "Vestidos", "Chaquetas", "Overoles", "Gorras", "Bolsos"];
 var imgGrandes = ["images/caramng14.jpg","images/caramng15.jpg","images/caramng8.jpg"];
-var imgPequeñas = ["images/caa.jpg","images/cama.jpg","images/cach.jpg","images/cao.jpg","images/cag.jpg","images/cave.jpg"];
+var imgPeque = ["images/caa.jpg","images/cama.jpg","images/cach.jpg","images/cao.jpg","images/cag.jpg","images/cave.jpg"];
 var precios = [270, 220, 120, 70, 20, 200 ];
 var stock = [8, 15, 11, 3, 10, 22];
 var precioTransporte = [6, 12, 20, "gratis"];
 var IVA = 0.18;
-var uniser;
+var uniUser;
+
+
+//JAVASCRIPT A EJECUTARSE UNA VEZ CARGADA LA PAGINA:
+	window.onload = function(){
+
+
+		//Se cargan los productos dentro del HTML de forna dinamica haciendo uso de los datos de la base de datos, como si de un PHP se tratase:
+		var DIVS = document.getElementsByName("DIVS");
+		for (i in productos){
+			DIVS[i].innerHTML = '<a id="imgG'+i+'" href="' +imgGrandes[i]+ '"><img id="imgP'+i+'" class="imagen" src="' +imgPeque[i]+ '"></a><div class="etiquetas"><b><span id="pro'+i+'">' +productos[i]+ '</span>: <span id="pre'+i+'">' +precios[i]+ '€</span></b></div><div class="stock">Hay en stock <span id="uni'+i+'">' +stock[i]+ '</span> unidades,<br/>¿Cuantas quiere?: <input class="uniBien" type="number" id="uniUser'+i+'" name="uniUser" value="0" size="4" /></div>';
+		}
+
+
+		//Rellena el campo dia y año, de la fecha de nacimiento y tarjeta de credito:
+		//Mas info en: http://www.tallerwebmaster.com/tutorial/mostrar-fecha-actual-con-javascrip/58/
+		//Fecha de nacimiento
+		var fecha = new Date();
+		var anio = fecha.getFullYear();
+
+		for (var i=1;i<=31;i++){
+			document.getElementById("fechaNacimientoDia").innerHTML = document.getElementById("fechaNacimientoDia").innerHTML + '<option value="' +i+ '">' +i+ '</option>';
+		}
+
+		for (var i=anio;i>=(anio-110);i--){
+			document.getElementById("fechaNacimientoAnio").innerHTML = document.getElementById("fechaNacimientoAnio").innerHTML + '<option value="' +i+ '">' +i+ '</option>';
+		}
+
+		//Tarjeta de credito:
+		for (var i=1;i<=12;i++){
+			document.getElementById("mesTarjeta").innerHTML = document.getElementById("mesTarjeta").innerHTML + '<option value="' +i+ '">' +i+ '</option>';
+		}
+
+		for (var i=anio;i<=(anio+21);i++){
+			document.getElementById("anioTarjeta").innerHTML = document.getElementById("anioTarjeta").innerHTML + '<option value="' +i+ '">' +i+ '</option>';
+		}
+
+
+
+		//Botones que llevaran a cabo la ejecucion de determinadas secuencias de codigo JavaScript:
+		document.getElementById("botonTotal").onclick = validaLasUnidades;
+		document.getElementById("botonDatos").onclick = pideDatos;
+		document.getElementById("botonPago").onclick = validaDatosPersonales;
+		document.getElementById("botonConfirmar").onclick = validaDatosPago;
+	}
