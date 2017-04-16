7cdfac1285929dae98077e48a5ef35b56d94c360
Angie Vanegas <arco527@gmail.com>
2017-04-16 00:47:55 -0500

    Pages Working

--- js/portada.js
+++ js/portada.js
@@ -21,37 +21,72 @@
 					elementos.pass2.value = "";
 					elementos.pass.className = elementos.pass.className + " error";
 					elementos.pass2.className = elementos.pass2.className + " error";
-				}else{
+				} else {
 					elementos.pass.className = elementos.pass.className.replace(" error", "");
 					elementos.pass2.className = elementos.pass2.className.replace(" error", "");
 				}
-				return true;
+			return true;
 			};
 
 			var validarRadios = function(){
-				var opciones = document.getElementsByName("sexo"),
+				var opciones = document.getElementsByName('sexo'),
 					resultado = false;
 
-					for(var i = 0; i< elementos.length; i++){
+					for(var i = 0; i < elementos.length; i++){
 						if (elementos[i].type == "radio" && elementos[i].name == "sexo"){
-							for (var o = 0; o < opciones; o++){
+							for (var o = 0; o < opciones.length; o++){
 								if(opciones[o].checked){
 									resultado = true;
 									break;
 								}
 							}
-							if (resultado == false){
-								elementos[i].parentNode.className = elementos[i].parentNode.className + " error";
-								console.log("el campo sexo esta incompleto");
-								return false;
-							} else {
-								elementos[i].parentNode.className = elementos[i].parentNode.replace(" error", "")
-									return true;
-							}
-						}
-					}
+									if (resultado == false){
+										elementos[i].parentNode.className = elementos[i].parentNode.className + " error";
+										console.log("el campo sexo esta incompleto");
+										return false;
+									} else {
+											elementos[i].parentNode.className = elementos[i].parentNode.className.replace(" error", "");
+											return true;
+										}
+						 }
+			 	  }
+			};
 
-			}
+
+
+			var validarCheckbox = function(){
+				var opciones = document.getElementsByName('terminos'),
+					resultado = false;
+
+					for(var i = 0; i < elementos.length; i++){
+						if (elementos[i].type == "checkbox"){
+							for (var o = 0; o < opciones.length; o++){
+								if(opciones[o].checked){
+									resultado = true;
+									break;
+								}
+							}
+									if (resultado == false){
+										elementos[i].parentNode.className = elementos[i].parentNode.className + " error";
+										console.log("el campo checkbox esta incompleto");
+										return false;
+									} else {
+											elementos[i].parentNode.className = elementos[i].parentNode.className.replace(" error", "");
+											return true;
+									}
+						 }
+			 	  }
+			};
+
+			// validar envio
+
+			var destino = "index.html";
+
+			function redireccionar(){
+				location.href= destino;
+			};
+
+
 
 			var enviar = function(e){
 				if(!validarInputs()){
@@ -63,11 +98,15 @@
 				}else if(!validarCheckbox()){
 					console.log('Falto validar los checkbox');
 					e.preventDefault();
-				}else{
+				}else if (!reedireccionar()){
 					console.log('Envia correctamente')
 					// comentar la línea cuando el formulario este listo
-				} e.preventDefault();
-			};
+				 //e.preventDefault();
+				}else {
+				console.log("no se a podido enviar")
+			}
+		}
+
 
 			// Funciones Blur y focus
 			var focusInput = function(){
@@ -86,8 +125,141 @@
 			// Eventos
 			formulario.addEventListener("submit", enviar);
 			for (var i = 0; i < elementos.length; i++){
-				if(elementos[i].type == "text" || elementos[i].type == "email" || elementos[i].type == "password")
+				if(elementos[i].type == "text" || elementos[i].type == "email" || elementos[i].type == "password"){
 				elementos[i].addEventListener("focus", focusInput);
 				elementos[i].addEventListener("blur", blurInput);
 			}
+		}
 }())
+
+
+//
+// // Recorrer los elementos y hacer que onchange ejecute una funcion para comprobar el valor de ese input
+// (function(){
+//
+// var formulario = document.formulario_registro,
+// 	elementos = formulario.elements;
+//
+// // Funcion que se ejecuta cuando el evento click es activado
+//
+// var validarInputs = function(){
+// 	for (var i = 0; i < elementos.length; i++) {
+// 		// Identificamos si el elemento es de tipo texto, email, password, radio o checkbox
+// 		if (elementos[i].type == "text" || elementos[i].type == "email" || elementos[i].type == "password") {
+// 			// Si es tipo texto, email o password vamos a comprobar que esten completados los input
+// 			if (elementos[i].value.length == 0) {
+// 				console.log('El campo ' + elementos[i].name + ' esta incompleto');
+// 				elementos[i].className = elementos[i].className + " error";
+// 				return false;
+// 			} else {
+// 				elementos[i].className = elementos[i].className.replace(" error", "");
+// 			}
+// 		}
+// 	}
+//
+// 	// Comprobando que las contraseñas coincidan
+// 	if (elementos.pass.value !== elementos.pass2.value) {
+// 		elementos.pass.value = "";
+// 		elementos.pass2.value = "";
+// 		elementos.pass.className = elementos.pass.className + " error";
+// 		elementos.pass2.className = elementos.pass2.className + " error";
+// 	} else {
+// 		elementos.pass.className = elementos.pass.className.replace(" error", "");
+// 		elementos.pass2.className = elementos.pass2.className.replace(" error", "");
+// 	}
+//
+// 	return true;
+// };
+//
+// var validarRadios = function(){
+// 	var opciones = document.getElementsByName('sexo'),
+// 		resultado = false;
+//
+// 	for (var i = 0; i < elementos.length; i++) {
+// 		if(elementos[i].type == "radio" && elementos[i].name == "sexo"){
+// 			// Recorremos los radio button
+// 			for (var o = 0; o < opciones.length; o++) {
+// 				if (opciones[o].checked) {
+// 					resultado = true;
+// 					break;
+// 				}
+// 			}
+//
+// 			if (resultado == false) {
+// 				elementos[i].parentNode.className = elementos[i].parentNode.className + " error";
+// 				console.log('El campo sexo esta incompleto');
+// 				return false;
+// 			} else {
+// 				// Eliminamos la clase Error del radio button
+// 				elementos[i].parentNode.className = elementos[i].parentNode.className.replace(" error", "");
+// 				return true;
+// 			}
+// 		}
+// 	}
+// };
+//
+// var validarCheckbox = function(){
+// 	var opciones = document.getElementsByName('terminos'),
+// 		resultado = false;
+//
+// 	for (var i = 0; i < elementos.length; i++) {
+// 		if(elementos[i].type == "checkbox"){
+// 			for (var o = 0; o < opciones.length; o++) {
+// 				if (opciones[o].checked) {
+// 					resultado = true;
+// 					break;
+// 				}
+// 			}
+//
+// 			if (resultado == false) {
+// 				elementos[i].parentNode.className = elementos[i].parentNode.className + " error";
+// 				console.log('El campo checkbox esta incompleto');
+// 				return false;
+// 			} else {
+// 				// Eliminamos la clase Error del checkbox
+// 				elementos[i].parentNode.className = elementos[i].parentNode.className.replace(" error", "");
+// 				return true;
+// 			}
+// 		}
+// 	}
+// };
+//
+// var enviar = function(e){
+// 	if (!validarInputs()) {
+// 		console.log('Falto validar los Input');
+// 		e.preventDefault();
+// 	} else if (!validarRadios()) {
+// 		console.log('Falto validar los Radio Button');
+// 		e.preventDefault();
+// 	} else if (!validarCheckbox()) {
+// 		console.log('Falto validar Checkbox');
+// 		e.preventDefault();
+// 	} else {
+// 		console.log('Envia');
+// 		e.preventDefault();
+// 	}
+// };
+//
+// var focusInput = function(){
+// 	this.parentElement.children[1].className = "label active";
+// 	this.parentElement.children[0].className = this.parentElement.children[0].className.replace("error", "");
+// };
+//
+// var blurInput = function(){
+// 	if (this.value <= 0) {
+// 		this.parentElement.children[1].className = "label";
+// 		this.parentElement.children[0].className = this.parentElement.children[0].className + " error";
+// 	}
+// };
+//
+// // --- Eventos ---
+// formulario.addEventListener("submit", enviar);
+//
+// for (var i = 0; i < elementos.length; i++) {
+// 	if (elementos[i].type == "text" || elementos[i].type == "email" || elementos[i].type == "password") {
+// 		elementos[i].addEventListener("focus", focusInput);
+// 		elementos[i].addEventListener("blur", blurInput);
+// 	}
+// }
+//
+// }())
